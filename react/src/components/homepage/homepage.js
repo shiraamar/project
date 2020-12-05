51c7427152a8472e13e8f9f50a7d25d027df25ae
shiraamar <s0504154078@gmail.com>
2020-12-06 00:29:32 +0200

    6/12/2020

--- react/src/components/homepage/homepage.js
+++ react/src/components/homepage/homepage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import  Header  from '../header/header.js'
+import Home from '../../assets/shutterstock_231955507.jpg'
 
 const HomePage = () => 
  // שם
@@ -11,7 +12,8 @@ const HomePage = () =>
     return (
         <div>
             
-     
+            <img src={Home} style={{ width: "100vw", height: "150vh" }}></img>‏
+
         {/* <Link to='/login' className="login" >התחבר</Link>
         <Link to='/sign' className="sign in">הרשם</Link> */}
         
